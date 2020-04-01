ad3945ad74a6cd12a20b746ff462e8d9c434f058
Josh Bryant <Kaytal@gmail.com>
2020-04-01 07:58:56 -0500

    Added Google Analytics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,8 +45,8 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-30027142-1",
-        head: true,
+        trackingId: "UA-162409003-1",
+        head: false,
       }
     },
     `gatsby-plugin-sass`, 
